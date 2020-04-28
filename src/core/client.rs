a53b1611ae53da630dd49e85c031825dd656680a
Alekos Filini <alekos.filini@gmail.com>
2020-04-28 09:41:36 +0200

    Implement std::error::Error

--- src/core/client.rs
+++ src/core/client.rs
@@ -206,6 +206,15 @@ pub enum Error {
     URLEncode(serde_urlencoded::ser::Error),
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {
+}
+
 impl From<reqwest::Error> for Error {
     fn from(other: reqwest::Error) -> Error {
         Error::Request(other)
